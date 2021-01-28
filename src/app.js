00f61f5e53d7c336b2558cf1fae69778248dd20a
李委洋 <15658150268@163.com>
2021-01-28 10:41:43 +0800

    使用 chai.expect 创建4个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ Vue.component('w-button', Button)
 Vue.component('w-icon', Icon)
 Vue.component('w-button-group',ButtonGroup)
 
+import chai from 'chai'
+const expect = chai.expect
+
 new Vue({
   el: '#app',
   data: {
@@ -14,3 +17,84 @@ new Vue({
     loading3: false,
   }
 })
+
+// 单元测试
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon:'settings'
+    }
+  })
+  vm.$mount()
+
+  const useElement = vm.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading:true
+    }
+  })
+  vm.$mount()
+
+  const useElement = vm.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount(div)
+
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition:'right'
+    }
+  })
+  vm.$mount(div)
+
+  let svg = vm.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount()
+  vm.$on('click', function () {
+    expect(1).to.eq(1)
+  })
+  let button = vm.$el
+  console.log(button)
+  button.click()
+}
